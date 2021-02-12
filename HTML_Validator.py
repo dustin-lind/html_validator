3dccab2822611f9ab13eaf4ce86284cbc401421b
Dustin Lind <Dustin.Lind.22@cmc.edu>
2021-02-11 17:03:33 -0800

    Complete Python functions

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 
-
 def validate_html(html):
     '''
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
@@ -10,6 +9,25 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
+    stack = []
+    if '<' not in html and '>' not in html:
+        return True
+    else:
+        extracted_tags = _extract_tags(html)
+        if len(extracted_tags) == 0:
+            return False
+        for i, tags in enumerate(extracted_tags):
+            if '/' not in tags:
+                stack.append(tags[1:-1])
+            else:
+                if len(stack) == 0:
+                    return False
+                elif stack[-1] == tags[2:-1]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+
 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
@@ -29,3 +47,29 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+
+    stack = []
+    word = ""
+    in_tag = False
+    attribute = False
+    for i, char in enumerate(html):
+        if char == '<':
+            if len(word) > 0:
+                word = ""
+                in_tag = False
+            else:
+                word += char
+                in_tag = True
+        elif char == '>':
+            if in_tag:
+                word += char
+                stack.append(word)
+                word = ""
+                in_tag = False
+                attribute = False
+        elif in_tag:
+            if char == ' ':
+                attribute = True
+            if not attribute: 
+                word += char
+    return stack
